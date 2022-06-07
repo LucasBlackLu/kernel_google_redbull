143558f11cd002fb3a2ae34f8045bcddafc98606
Juhyung Park <qkrwngud825@gmail.com>
2022-06-07 18:04:06 +0900

    lrng: merge v45
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Change-Id: I02c4ffc3be466ef3879dc5d88439e7683fc677f8

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5,6 +5,8 @@
  *
  *  Copyright (C) 1991-2002  Linus Torvalds
  */
+#include <linux/lrng.h>
+
 #include "sched.h"
 
 #include <linux/nospec.h>
@@ -2369,6 +2371,8 @@ static inline int __set_cpus_allowed_ptr(struct task_struct *p,
 static void ttwu_stat(struct task_struct *p, int cpu, int wake_flags)
 {
 	struct rq *rq;
+
+	add_sched_randomness(p, cpu);
 
 	if (!schedstat_enabled())
 		return;
